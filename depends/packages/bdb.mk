723525de9f61bfcf7c06e367ebeb7be4ebe4d1fb
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-05-01 15:18:03 +0200

    Update bdb.mk

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -1,10 +1,9 @@
 package=bdb
-$(package)_version=4.8.30
+$(package)_version=5.3.28
 $(package)_download_path=https://download.oracle.com/berkeley-db
-$(package)_file_name=db-$($(package)_version).NC.tar.gz
-$(package)_sha256_hash=12edc0df75bf9abd7f82f821795bcee50f42cb2e5f76a6a281b85732798364ef
+$(package)_file_name=db-$($(package)_version).tar.gz
+$(package)_sha256_hash=e0a992d740709892e81f9d93f06daf305cf73fb81b545afe72478043172c3628
 $(package)_build_subdir=build_unix
-$(package)_patches=clang_cxx_11.patch
 
 define $(package)_set_vars
 $(package)_config_opts=--disable-shared --enable-cxx --disable-replication --enable-option-checking
@@ -20,7 +19,9 @@ $(package)_cppflags_mingw32=-DUNICODE -D_UNICODE
 endef
 
 define $(package)_preprocess_cmds
-  patch -p1 < $($(package)_patch_dir)/clang_cxx_11.patch && \
+  sed -i.old 's/WinIoCtl.h/winioctl.h/g' src/dbinc/win_db.h && \
+  sed -i.old 's/__atomic_compare_exchange\\(/__atomic_compare_exchange_db(/' src/dbinc/atomic.h && \
+  sed -i.old 's/atomic_init/atomic_init_db/' src/dbinc/atomic.h src/mp/mp_region.c src/mp/mp_mvcc.c src/mp/mp_fget.c src/mutex/mut_method.c src/mutex/mut_tas.c && \
   cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist
 endef
 
@@ -29,7 +30,7 @@ define $(package)_config_cmds
 endef
 
 define $(package)_build_cmds
-  $(MAKE) libdb_cxx-4.8.a libdb-4.8.a
+  $(MAKE) libdb_cxx-5.3.a libdb-5.3.a
 endef
 
 define $(package)_stage_cmds
